8dcabd9500d37dea4f38122e033f2a242059445f
kokiheey <rivilttm@gmail.com>
2026-08-01 22:59:53 +0200

    go

---
+++ Scenes/gradient_game_manager.gd
@@ -0,0 +1,17 @@
+extends minigame
+
+var finishedPanelStyle : StyleBoxFlat
+@onready var hBox := $Panel/HBoxContainer
+
+func _ready():
+	hBox.gameFinished.connect(_onGameFinished)
+	
+	finishedPanelStyle = $Panel.get_theme_stylebox("panel")
+	finishedPanelStyle.border_color = Color(0,0,0,0)
+
+func _start() -> void:
+	hBox.gameFinished.connect(_onGameFinished)
+
+func _onGameFinished():
+	finishedPanelStyle.border_color = Color.WHITE
+	finish(true)
--- Scripts/gradient_game.gd
+++ Scripts/gradient_game.gd
@@ -1,5 +1,6 @@
 extends HBoxContainer
 
+signal gameFinished
 var controls : Array[Control]
 var numberOfTiles : int
 var leftColor : Color
@@ -9,6 +10,10 @@ func _process(_delta: float) -> void:
 		if controls[i] != get_child(i):
 			return
 	
+	gameFinished.emit()
+	
+func color_distance(a: Color, b: Color) -> float:
+	return sqrt(pow(a.r - b.r, 2) + pow(a.g - b.g, 2) + pow(a.b - b.b, 2))
 
 func _ready():
 	controls.append($Control)
@@ -16,9 +21,17 @@ func _ready():
 	leftColor = Color(0.5 + randf()/2.0, 0.5 + randf()/2.0\
 	, 0.5 + randf()/2.0)
 	
-	rightColor = leftColor.blend(Color(0.5 + randf()/2.0, 0.5\
+	rightColor = leftColor.lerp(Color(0.5 + randf()/2.0, 0.5\
 	 + randf()/2.0\
-	, 0.5 + randf()/2.0, 0.7))
+	, 0.5 + randf()/2.0), 0.5)
+	
+	var count = 0
+	while color_distance(leftColor, rightColor) < 0.3 and count < 10:
+		rightColor = leftColor.lerp(Color(0.3 + randf()/2.0, 0.3\
+		 + randf()/2.0\
+		, 0.3 + randf()/2.0), 0.5)
+		print(color_distance(leftColor, rightColor))
+		count += 1
 	
 	var panel = controls.back().get_child(0)
 	var styleBox: StyleBoxFlat = panel.get_theme_stylebox("panel").duplicate()
@@ -32,5 +45,7 @@ func _ready():
 		styleBox.set("bg_color", leftColor.lerp(rightColor, 1.0*controls.size()/numberOfTiles))
 		panel.add_theme_stylebox_override("panel", styleBox)
 	
+	controls.front().set_script(null)
+	controls.back().set_script(null)
 	for i in range(get_child_count()-2, 0, -1):
 		move_child(get_child(i), randi_range(1, i))
